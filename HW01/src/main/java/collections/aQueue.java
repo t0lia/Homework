0f7b9d54581036d7621a6fa6c90f8161b5b7d0aa
t0lia <t0lia@yandex.ru>
2015-12-21 20:48:53 +0300

    adding queue implementation

--- HW01/src/main/java/collections/aQueue.java
+++ HW01/src/main/java/collections/aQueue.java
@@ -1,26 +1,126 @@
 package main.java.collections;
 
+import java.util.NoSuchElementException;
+
 /**
  * Created by t0lia on 19.12.15.
  */
 public class aQueue<T> implements IQueue<T> {
+    private static final int INITIAL_CAPACITY = 16;
+    private T[] container;
+    private int capacity;
+    private int head, tail;
+    private int size;
+
+    @SuppressWarnings("unchecked")
+    public aQueue() {
+        container = (T[]) new Object[INITIAL_CAPACITY];
+        capacity = INITIAL_CAPACITY;
+        head = tail = -1;
+        size = 0;
+    }
+
     @Override
     public void add(T t) {
+        if (t == null) {
+            throw new NullPointerException();
+        }
 
+        if (size == capacity) {
+            resize(capacity*2);
+            tail++;
+        } else if (size == 0) {
+            tail = head = 0;
+        } else {
+            tail = (tail + 1) % capacity;
+        }
+        container[tail] = t;
+        size++;
     }
 
     @Override
     public T remove() {
-        return null;
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        if (size < capacity/4) {
+            resize(capacity/2);
+        }
+
+        T result = container[head];
+        container[head] = null;
+        head = (head + 1) % capacity;
+        size--;
+        return result;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
+
+    }
+
+    private void print() {
+        for (T t : container) {
+            if (t != null) {
+                System.out.print(t.toString() + " ");
+            } else {
+                System.out.print("_ ");
+            }
+        }
+        System.out.println();
+    }
+
+    private void resize(int copyCapacity) {
+        T[] copy = (T[]) new Object[copyCapacity];
+        capacity = copyCapacity;
+
+        for (int i = 0, j = head; i < size; i++) {
+            copy[i] = container[head];
+            head++;
+            head %= capacity;
+        }
+
+
+        container = copy;
+        head= 0;
+        tail = size - 1;
+
+
+    }
+
+    public static void main(String[] args) {
+        aQueue<Integer> q = new aQueue<Integer>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.print();
+        q.add(6);
+        q.print();
+        q.remove();
+        q.print();
+        q.remove();
+        q.print();
+        q.remove();
+        q.print();
+        q.remove();
+        q.print();
+        q.remove();
+        q.print();
+        q.remove();
+        q.print();
+//        q.add(5);
+        //System.out.println( q.remove());
+//        for (int i = 0; i < q.container.length; i ++) {
+//            System.out.println(q.container[i]);
+//        }
+
     }
 }
--- HW01/src/main/java/collections/lQueue.java
+++ HW01/src/main/java/collections/lQueue.java
@@ -1,26 +1,61 @@
 package main.java.collections;
 
+import java.util.NoSuchElementException;
+
 /**
  * Created by t0lia on 19.12.15.
  */
 public class lQueue<T> implements IQueue<T>{
+    private Node<T> head, tail;
+    private int size;
+
+
+    public lQueue() {
+        head = tail = new Node<>();
+        size = 0;
+    }
+
     @Override
     public void add(T t) {
+        if (t == null) {
+            throw new NullPointerException();
+        }
 
+        Node<T> node = new Node<>();
+        node.elem = t;
+
+        tail.next = node;
+
+        tail = node;
+        size++;
     }
 
     @Override
     public T remove() {
-        return null;
+        if (size == 0) {
+            throw new NoSuchElementException();
+        }
+        T result = head.next.elem;
+        head.next.elem = null;
+
+        head = head.next;
+
+        return result;
+
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
+    }
+
+    class Node<T> {
+        T elem;
+        Node<T> next;
     }
 }
